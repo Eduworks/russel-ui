2909e48cffc805903e2d03b670b903bc6c77c6b8
winonadeb <winonadeb@users.noreply.github.com>
2015-09-24 17:26:16 -0500

    placeholder classes

---
+++ russel-ui/src/com/eduworks/russel/ui/client/ModalDispatch.java
@@ -0,0 +1,7 @@
+package com.eduworks.russel.ui.client;
+
+
+public class ModalDispatch extends com.eduworks.gwt.client.pagebuilder.modal.ModalDispatch {
+	
+
+}
---
+++ russel-ui/src/com/eduworks/russel/ui/client/OverlayDispatch.java
@@ -0,0 +1,7 @@
+package com.eduworks.russel.ui.client;
+
+
+public class OverlayDispatch extends com.eduworks.gwt.client.pagebuilder.overlay.OverlayDispatch {
+	
+
+}
